828d0a30ef6adc7959b5798f29020dabb381db9b
vokhanhan25 <vokhanhan25@gmal.com>
2020-01-23 08:18:20 +0700

    IMPROV Cat eat method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 var Cat = require ('./cat');
+var Mouse = require ('./mouse');
+var Dog = require ('./dog');
+
+var cat = new Cat ();
+var mouse = new Mouse ('Mickey');
+var dog = new Dog ();
+
+try {
+    cat.eat(dog);
+} catch (error) {
+    console.log ('Error while Cat eating Dog');
+}
+console.log (cat);
